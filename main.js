d69057f5350e088958541682c92d83c458b5abc7
Mohamed Amine Lejmi <lejmi.amine@gmail.com>
2018-10-23 17:03:15 +0100

    V3.1 Gestion des pyramides négatives :)

--- main.js
+++ main.js
@@ -6,21 +6,31 @@ do {
     var nombreEtages = prompt("Combien d'étages veux-tu ?");
     if (nombreEtages > 25) {
         alert("Le nombre d'étages doit être inférieur à 25 !");
-    } else if(nombreEtages <= 0){
-        alert('Une pyramide ne pêut pas avoir de sous-sol :p');
-    } else if(isNaN(nombreEtages)) {
+    } else if (nombreEtages == 0) {
+        alert('Le nombre d\'étages ne peut pas être nul');
+    } else if (isNaN(nombreEtages)) {
         alert('La valeur d\'entrée doit être numérique');
     }
-} while (nombreEtages > 25 || nombreEtages <= 0 || isNaN(nombreEtages) )
+} while (nombreEtages > 25 || nombreEtages == 0 || isNaN(nombreEtages))
 
 
 console.log('Voici la pyramide :');
 
 let result = '';
-for (let i = 1; i <= nombreEtages; i++) {
-    let etage = '';
-    etage += (' ').repeat(nombreEtages - i) + '#'.repeat(2 * i - 1);
-    result += etage + '\n';
+let nombreEtagesPos = Math.abs(nombreEtages);
+
+if (nombreEtages > 0) {
+    for (let i = 1; i <= nombreEtagesPos; i++) {
+        let etage = '';
+        etage += (' ').repeat(nombreEtagesPos - i) + '#'.repeat(2 * i - 1);
+        result += etage + '\n';
+    }
+} else {
+    for (let i = nombreEtagesPos; i >= 1; i--) {
+        let etage = '';
+        etage += (' ').repeat(nombreEtagesPos - i) + '#'.repeat(2 * i - 1);
+        result += etage + '\n';
+    }
 }
 console.log(result);
 
